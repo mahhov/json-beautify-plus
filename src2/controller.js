a2d9ef49c80d62c1412aab696fbcdff969c29537
mahhov <mahhov1@gmail.com>
2018-05-07 09:46:16 -0700

    deleting and creating and selecting pairs

--- src2/controller.js
+++ src2/controller.js
@@ -1,19 +1,35 @@
 const source = require('bb-better-binding')(__dirname, document);
 
+// tabs
+
 source.createPair = () => {
     let name = source.pairs.length;
     let pair = {name, raw: ''};
     source.pairs.push(pair);
-    source.setActivePair(pair);
+    source.setActivePairIndex(source.pairs.length - 1);
     saveChanges();
     source.rawElem.focus();
 };
 
-source.setActivePair = pair => {
-    source.activePair = pair;
-    source.rawElem.value = pair.raw;
+source.setActivePairIndex = index => {
+    source.activePairIndex = index;
+    source.rawElem.value = getActivePair().raw;
     updatePretty();
 };
+
+source.deletePair = index => {
+    if (source.pairs.length === 1)
+        init();
+    else {
+        source.pairs.splice(index, 1);
+        if (source.activePairIndex === index)
+            source.setActivePairIndex(index < source.pairs.length ? index : source.pairs.length - 1);
+        else if (source.activePairIndex > index)
+            source.setActivePairIndex(source.activePairIndex - 1);
+    }
+};
+
+// change in text
 
 source.rawChanged = () => {
     saveChanges();
@@ -21,15 +37,19 @@ source.rawChanged = () => {
 };
 
 let saveChanges = () => {
-    source.activePair.raw = source.rawElem.value;
+    getActivePair().raw = source.rawElem.value;
     console.log('todo - save');
 };
 
-let updatePretty = () => source.pretty = source.activePair.raw;
+let updatePretty = () => source.pretty = getActivePair().raw;
+
+let getActivePair = () => source.pairs[source.activePairIndex];
 
 let init = () => {
     source.pairs = [];
     source.createPair();
-    source.setActivePair(source.pairs[0]);
+    source.setActivePairIndex(0);
 };
 init();
+
+// todo focus text on load
